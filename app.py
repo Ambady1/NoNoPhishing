d429afe948fe927c6455bfc524382eb80becbbbf
Ambady <cssreedeep@gmail.com>
2025-01-24 10:28:17 +0530

    Fixx

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ import joblib
 import requests
 
 # Load Models
-urlnet_model = load_model("urlnet_model.h5")
+urlnet_model = load_model("urlnet_model.h5",compile=False)
 htmlphish_model = load_model("htmlphish_model.h5", compile=False)
-dom2vec_model = Doc2Vec.load("dom2vec_model")
-stacking_model = joblib.load("stacking_model.pkl")
+dom2vec_model = Doc2Vec.load("dom2vec_model",compile=False)
+stacking_model = joblib.load("stacking_model.pkl",compile=False)
 
 # Preprocess URL Function
 
